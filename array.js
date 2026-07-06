3905e447d64df702d8063c6d4a33b128a0f9290f
Zahida-Siddique <zahiaaa550@gmail.com>
2026-07-06 10:57:24 -0700

    Add JavaScript assignment

--- array.js
+++ array.js
@@ -1,23 +1,40 @@
-const products = [
-    {
-    name: "apple",
-    price: 100,
-    stock: 5
-},
-{
-    name: "mango",
-    price: 200,
-    stock: 0
+// const products = [
+//     {
+//     name: "apple",
+//     price: 100,
+//     stock: 5
+// },
+// {
+//     name: "mango",
+//     price: 200,
+//     stock: 0
 
-},
-{
+// },
+// {
 
-    name: "orange",
-    price: 150,
-    stock: 3
-}
-];
-const availableProducts = products.find(function(products){
-    return products.name === "apple";
-});
-console.log(availableProducts);
+//     name: "orange",
+//     price: 150,
+//     stock: 3
+// }
+// ];
+// const availableProducts = products.find(function(products){
+//     return products.name === "apple";
+// });
+// console.log(availableProducts);
+
+
+const redius = 7;
+const area = Math.PI * redius;
+
+console.log(area);
+
+
+
+
+
+
+
+
+
+
+
